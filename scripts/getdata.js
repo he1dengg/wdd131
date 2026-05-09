4999bb08ba24c8e7a8a9f17d63511a6334583bb7
he1dengg <thangnhokpct2k5@gmail.com>
2026-05-09 23:24:01 +0700

    Update getdata.js

--- scripts/getdata.js
+++ scripts/getdata.js
@@ -3,3 +3,6 @@ yearSpan.textContent = new Date().getFullYear();
 
 const lastModParagraph = document.querySelector("#lastModified");
 lastModParagraph.textContent = `Last Modification: ${document.lastModified}`;
+
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = "Last Modification: " + document.lastModified;
